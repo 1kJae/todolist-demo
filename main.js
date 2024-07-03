0306621c9ba29b4ffd763cd1187be738246bee62
1kJae <wnstjq62@naver.com>
2024-07-03 23:18:55 +0900

    second commut

--- main.js
+++ main.js
@@ -13,28 +13,75 @@
 // 끝남탭은, 끝난 아이템만, 진행중 탭은 진행중인 아이템만
 // 전체탭을 누르면 다시 전체아이템으로 돌아옴
 
+// 1. check 버튼을 클릭하는 순간 true false
+// 2. true이면 끝난 걸로 간주하고 밑줄 보여주기
+// 3. false이면 안 끝난 걸로 간주하고 그대로
+
+// 1.check와 delete가 아이콘이어야 함 O
+// 2.check버튼 클릭시 뒤에 배경이 회색으로 바뀌어야함
+// 3.check버튼 클릭 후 되돌리기 버튼이 나오고 클릭하면 뒤에 배경이 다시 돌아오고 버튼도 다시 체크로 바꿈
+// 4.삭제기능이 있어야함 O
+
 let taskInput = document.getElementById("task-input");
 let addButton = document.getElementById("add-button");
 let taskList = []
 addButton.addEventListener("click", addTask)
 
 function addTask() {
-    let taskContent = taskInput.value
-    taskList.push(taskContent)
+    let task = {
+        id: randomIDGenerate(),
+        taskContent: taskInput.value,
+        isComplete: false
+    }
+    taskList.push(task)
     render();
 }
 
 function render() {
     let resultHTML = "";
     for (let i = 0; i < taskList.length; i++) {
-        resultHTML += `<div class="task">
-                    <div>${taskList[i]}</div>
+        if (taskList[i].isComplete == true) {
+            resultHTML += `<div class="task">
+                    <div class="task-done">${taskList[i].taskContent}</div>
                     <div>
-                        <button>Check</button>
-                        <button>Delete</button>
+                        <button class="check-On-Btn" onclick="toggleComplete('${taskList[i].id}')"></button>
+                        <button class="delete-Btn" onclick="deleteTask('${taskList[i].id}')"></button>
                     </div>
                 </div>`;
+        } else {
+            resultHTML += `<div class="task">
+                    <div>${taskList[i].taskContent}</div>
+                    <div>
+                        <button class="check-Off-Btn" onclick="toggleComplete('${taskList[i].id}')"></button>
+                        <button class="delete-Btn" onclick="deleteTask('${taskList[i].id}')"></button>
+                    </div>
+                </div>`;
+        }
     }
 
-    document.getElementById("task-board").innerHTML = resultHTML;
+    document.getElementById("task-board").innerHTML = resultHTML;   
+}
+
+function toggleComplete(id) {
+    for (let i = 0; i < taskList.length; i++) {
+        if (taskList[i].id == id) {
+            taskList[i].isComplete = !taskList[i].isComplete;
+            break;
+        }
+    }
+    render()
+}
+
+function deleteTask(id) {
+    for (let i = 0; i < taskList.length; i++) {
+        if (taskList[i].id == id) {
+            taskList.splice(i, 1)
+            break;
+        }
+    }
+    render();
+}
+
+function randomIDGenerate() {
+    return '_' + Math.random().toString(36).substr(2, 9);
 }
